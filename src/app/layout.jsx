90378a911315800fd23e7e043fd64eb6e6b8edb1
sathish kumar <sathizzkumarr@gmail.com>
2025-08-26 20:25:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,7 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
@@ -31,6 +31,7 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body
+        suppressHydrationWarning={true}
         className={cn(
           "min-h-screen bg-background font-body antialiased",
           fontInter.variable
