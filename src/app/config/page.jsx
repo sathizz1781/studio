19653df3594afe9e085836b51f7b595672821aae
sathish kumar <sathizzkumarr@gmail.com>
2025-11-19 18:58:59 +0000

    Page getting braek when i go to config page

--- src/app/config/page.jsx
+++ src/app/config/page.jsx
@@ -23,6 +23,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Badge } from "@/components/ui/badge";
 import { useToast } from "@/hooks/use-toast";
 import { Loader2, Save, AlertCircle, CalendarClock, Phone } from "lucide-react";
